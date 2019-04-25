4534c2d359fe7568b1ae47e17cd566c634b701ed
Artem GORBOUNOV <artem.gorbounov@gmail.com>
2019-04-25 11:53:11 +0200

    Update entity beans/Itinerary.java

--- src/main/java/fr/kacetal/escalade/model/beans/Itinerary.java
+++ src/main/java/fr/kacetal/escalade/model/beans/Itinerary.java
@@ -1,5 +1,26 @@
 package fr.kacetal.escalade.model.beans;
 
-//Voie
+import javax.persistence.*;
+
+@Entity
 public class Itinerary {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Sector sector;
+    
+    @Enumerated(value = EnumType.STRING)
+    private Grade grade;
+    
+    @Column(nullable = false)
+    private String name;
+    
+    @Column(nullable = false)
+    private Integer length;
+    
+    @Column(length = 1000)
+    private String description;
 }
