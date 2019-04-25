78d35af41cb4dcb1644432f3b460af7acaf74f27
Artem GORBOUNOV <artem.gorbounov@gmail.com>
2019-04-25 11:55:22 +0200

    Update entity beans/Sector.java

--- src/main/java/fr/kacetal/escalade/model/beans/Sector.java
+++ src/main/java/fr/kacetal/escalade/model/beans/Sector.java
@@ -1,5 +1,25 @@
 package fr.kacetal.escalade.model.beans;
 
-//Sectuer
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Sector {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Site site;
+    
+    @Column(nullable = false)
+    private String name;
+    
+    @Column(length = 1000)
+    private String description;
 }
