25d7ac22f5acc4d1b7b69cbe4563955e9ef7d284
Artem GORBOUNOV <artem.gorbounov@gmail.com>
2019-04-25 11:56:19 +0200

    Update entity beans/Site.java

--- src/main/java/fr/kacetal/escalade/model/beans/Site.java
+++ src/main/java/fr/kacetal/escalade/model/beans/Site.java
@@ -1,6 +1,21 @@
 package fr.kacetal.escalade.model.beans;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class Site {
-
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false)
+    private String name;
+    
+    @Column(length = 1000)
+    private String description;
 }
