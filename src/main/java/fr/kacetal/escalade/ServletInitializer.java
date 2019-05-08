7944f4f2ed54b0322f68b40f5a468eceaa3346ee
Artem GORBOUNOV <artem.gorbounov@gmail.com>
2019-05-08 15:55:27 +0200

    Add ServletInitializer

--- src/main/java/fr/kacetal/escalade/ServletInitializer.java
+++ src/main/java/fr/kacetal/escalade/ServletInitializer.java
@@ -1,13 +1,13 @@
+
 package fr.kacetal.escalade;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(EscaladeApplication.class);
-	}
-
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EscaladeApplication.class);
+    }
 }
