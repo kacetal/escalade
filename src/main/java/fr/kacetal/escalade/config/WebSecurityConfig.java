4fb4d5f8e0d5fb443896d08f861663e1178b3812
Artem GORBOUNOV <artem.gorbounov@gmail.com>
2019-05-08 16:05:41 +0200

    Add config files MvcConfig and WebSecurityConfig

---
+++ src/main/java/fr/kacetal/escalade/config/WebSecurityConfig.java
@@ -0,0 +1,47 @@
+package fr.kacetal.escalade.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    
+    public void configure(WebSecurity webSecurity) {
+        webSecurity.ignoring().antMatchers("/static/**");
+    }
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/index").permitAll()
+                .and()
+                .authorizeRequests().antMatchers("/sites/**").authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+    
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        auth
+                .inMemoryAuthentication()
+                .passwordEncoder(passwordEncoder)
+                .withUser("user").password(passwordEncoder.encode("user")).roles("USER");
+        
+    }
+    
+}
