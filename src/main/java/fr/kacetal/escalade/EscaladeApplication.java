80745042e0114ebf0f2f37eb0d053198f897a79e
Artem GORBOUNOV <artem.gorbounov@gmail.com>
2019-05-08 16:06:32 +0200

    Change EscaladeApplication rearrange code

--- src/main/java/fr/kacetal/escalade/EscaladeApplication.java
+++ src/main/java/fr/kacetal/escalade/EscaladeApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EscaladeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EscaladeApplication.class, args);
 	}
-
 }
