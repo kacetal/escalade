65a35662175b45550929d5e1403ef850194ef136
Artem GORBOUNOV <artem.gorbounov@gmail.com>
2019-05-10 11:19:36 +0200

    Add SectorRepository

---
+++ src/main/java/fr/kacetal/escalade/persistence/repository/SectorRepository.java
@@ -0,0 +1,7 @@
+package fr.kacetal.escalade.persistence.repository;
+
+import fr.kacetal.escalade.persistence.entities.Sector;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SectorRepository extends CrudRepository<Sector, Long> {
+}
