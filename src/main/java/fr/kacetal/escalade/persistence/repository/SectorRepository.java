d24e8a250b35d3efe211b4c3f50e25697b21e090
Artem GORBOUNOV <artem.gorbounov@gmail.com>
2019-05-27 11:25:57 +0200

    Add methods for sectorRepository

--- src/main/java/fr/kacetal/escalade/persistence/repository/SectorRepository.java
+++ src/main/java/fr/kacetal/escalade/persistence/repository/SectorRepository.java
@@ -1,7 +1,12 @@
 package fr.kacetal.escalade.persistence.repository;
 
 import fr.kacetal.escalade.persistence.entities.Sector;
+import fr.kacetal.escalade.persistence.entities.Site;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface SectorRepository extends CrudRepository<Sector, Long> {
+    List<Sector> findSectorsByNameContainsIgnoreCase(String name);
+    List<Sector> findSectorsBySite(Site site);
 }
