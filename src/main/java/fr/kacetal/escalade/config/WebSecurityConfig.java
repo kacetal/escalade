078af6137d2e01c72e04c37904096138816307bb
Artem GORBOUNOV <artem.gorbounov@gmail.com>
2019-05-29 11:11:09 +0200

    Revert "Add "/index" path for matching"
    This reverts commit 4e113d81

--- src/main/java/fr/kacetal/escalade/config/WebSecurityConfig.java
+++ src/main/java/fr/kacetal/escalade/config/WebSecurityConfig.java
@@ -22,7 +22,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/index").permitAll()
+                .antMatchers("/").permitAll()
                 .and()
                 .authorizeRequests().antMatchers("/**").authenticated()
                 .and()
