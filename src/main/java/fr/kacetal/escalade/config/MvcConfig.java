786cb9f2867462181fe506a80bd44c8748931512
Artem GORBOUNOV <artem.gorbounov@gmail.com>
2019-06-22 23:45:02 +0200

    Add pattern for static image path

--- src/main/java/fr/kacetal/escalade/config/MvcConfig.java
+++ src/main/java/fr/kacetal/escalade/config/MvcConfig.java
@@ -1,5 +1,6 @@
 package fr.kacetal.escalade.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -9,6 +10,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
+    
+    @Value("${upload.path}")
+    private String uploadPath;
     
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
@@ -25,10 +29,15 @@ public class MvcConfig implements WebMvcConfigurer {
                     .addResourceLocations("classpath:/META-INF/resources/webjars/");
         }
         
-        if (!registry.hasMappingForPattern("/**")) {
-            registry
-                    .addResourceHandler("/**")
-                    .addResourceLocations("classpath:/META-INF/resources/**");
-        }
+//        if (!registry.hasMappingForPattern("/**")) {
+//            registry
+//                    .addResourceHandler("/**")
+//                    .addResourceLocations("classpath:/META-INF/resources/**");
+//        }
+    
+        registry.addResourceHandler("/img/**")
+                .addResourceLocations("classpath:/static/images/");
+//        registry.addResourceHandler("/static/**")
+//                .addResourceLocations("classpath:/static/");
     }
 }
